b733a402c453fc142e6847844e0044b8c88d52dc
Yasser Mohammed ElSayed <yasser.mohamed@enozom.com>
2023-08-22 13:34:22 +0300

    in this commit: 1) creating Stores Client

--- app/src/main/java/com/example/foodcourt/network/StoresClient.kt
+++ app/src/main/java/com/example/foodcourt/network/StoresClient.kt
@@ -1,10 +1,34 @@
+import com.example.foodcourt.model.StoresList
+import com.example.foodcourt.network.ApiService
+import com.example.foodcourt.network.RemoteService
+import com.example.foodcourt.network.RetrofitHelper
+
 //package com.example.foodcourt.network
 //
 //import com.example.foodcourt.model.StoresList
 //
-//class StoresClient:RemoteService {
-//    override suspend fun getAllStores(): StoresList {
-//
-//    }
-//
-//}
+class StoresClient: RemoteService {
+
+    val apiService: ApiService by lazy {
+        RetrofitHelper.retrofitInstance.create(ApiService::class.java)
+    }
+
+
+
+    companion object{
+        private var instance: StoresClient?=null
+        fun getInstance(): StoresClient {
+            return instance?: synchronized(this)
+            {
+                val temp= StoresClient()
+                instance=temp
+                temp
+            }
+        }
+    }
+    override suspend fun getAllStores(): StoresList {
+        val response =  apiService.getStores()
+        return response
+    }
+
+}
