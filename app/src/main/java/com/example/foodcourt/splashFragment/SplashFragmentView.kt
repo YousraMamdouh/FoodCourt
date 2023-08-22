57f122814e92dcd5b6677dedf397862c1586ff5b
Yasser Mohammed ElSayed <yasser.mohamed@enozom.com>
2023-08-22 11:09:08 +0300

    in this commit: creating project architecture

--- app/src/main/java/com/example/foodcourt/splashFragment/SplashFragmentView.kt
+++ app/src/main/java/com/example/foodcourt/splashFragment/SplashFragmentView.kt
@@ -1,10 +1,11 @@
-package com.example.foodcourt
+package com.example.foodcourt.splashFragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.foodcourt.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeFragmentView.newInstance] factory method to
+ * Use the [SplashFragmentView.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragmentView : Fragment() {
+class SplashFragmentView : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class HomeFragmentView : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_view, container, false)
+        return inflater.inflate(R.layout.fragment_splash, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class HomeFragmentView : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragmentView.
+         * @return A new instance of fragment SplashFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomeFragmentView().apply {
+            SplashFragmentView().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
